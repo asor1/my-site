861b8349dce71dabb3895e308a8551c8071e5792
asor <asor1176@gmail.com>
2017-02-01 17:50:36 +1000

    отредактировал сой урл в сетинг

--- pivo/settings.py
+++ pivo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd#)^i84k0oa^-nrj=eu+1nf^hd*+c5vkq6608ap$_$cynj1mfq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [asor1.pythonanywhere.com]
 
 
 # Application definition
